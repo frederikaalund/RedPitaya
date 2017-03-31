d5d8bec7d6e3dc74ecf8d616d9ab6323315a1434
Frederik Aalund <fpa@sbtaqua.com>
2017-03-31 16:51:02 +0200

    Upgraded settings file to reflect the newest toolsets.

--- settings.sh
+++ settings.sh
@@ -2,14 +2,14 @@
 # setup Xilinx Vivado FPGA tools
 ################################################################################
 
-. /opt/Xilinx/Vivado/2016.2/settings64.sh
-. /opt/Xilinx/SDK/2016.2/settings64.sh
+#. /opt/Xilinx/Vivado/2016.2/settings64.sh
+#. /opt/Xilinx/SDK/2016.2/settings64.sh
 
 ################################################################################
 # setup cross compiler toolchain
 ################################################################################
 
-export TOOLCHAIN_PATH=/opt/gcc-linaro-6.1.1-2016.08-x86_64_arm-linux-gnueabihf
+export TOOLCHAIN_PATH=/opt/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf
 export PATH=$PATH:$TOOLCHAIN_PATH/bin
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
